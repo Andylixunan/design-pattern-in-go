edcbcf38a31aebf485a5a69f99e5169c671212d4
LI Xunan <xl2216@nyu.edu>
2022-01-15 16:27:58 +0800

    add strategy pattern

---
+++ strategy/strategy.go
@@ -0,0 +1,39 @@
+package strategy
+
+import "fmt"
+
+type EvictionAlgo interface {
+	Evict(c *Cache)
+}
+
+type Cache struct {
+	EvictionAlgo EvictionAlgo
+	Storage      map[string]string
+}
+
+// To initialize a new cache, we need our clients to specify
+// a concrete eviction algorithm
+func NewCache(e EvictionAlgo) *Cache {
+	return &Cache{
+		EvictionAlgo: e,
+		Storage:      make(map[string]string),
+	}
+}
+
+func (c *Cache) SetEvictionAlgo(e EvictionAlgo) {
+	c.EvictionAlgo = e
+}
+
+// FIFO and LRU struct implement EvictionAlgo interface
+
+type FIFO struct{}
+
+func (f FIFO) Evict(c *Cache) {
+	fmt.Println("Evicting by fifo strategy")
+}
+
+type LRU struct{}
+
+func (l LRU) Evict(c *Cache) {
+	fmt.Println("Evicting by lru strategy")
+}
---
+++ strategy/strategy_test.go
@@ -0,0 +1,30 @@
+package strategy
+
+import (
+	"testing"
+)
+
+func TestNewCache(t *testing.T) {
+	type args struct {
+		e EvictionAlgo
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "testFIFO",
+			args: args{FIFO{}},
+		},
+		{
+			name: "testLRU",
+			args: args{LRU{}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewCache(tt.args.e)
+			got.EvictionAlgo.Evict(got)
+		})
+	}
+}
